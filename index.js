383f800af29d1f6a9fa81789292e0b7ddbf2332f
adihendro <adi21hendro@gmail.com>
2021-11-24 18:34:15 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const find = document.getElementById("button-find");
+find.addEventListener("click", postFind);
+
+async function postFind() {
+    // const username = form.username.value;
+    // const fullname = form.fullname.value;
+    // const email = form.email.value;
+    // const password = form.password.value;
+    // const data = {
+    //     "username": username,
+    //     "fullname": fullname,
+    //     "email": email,
+    //     "password": password,
+    //     "role": ["ROLE_CLIENT"]
+    // };
+    window.location = 'result.html'
+}
---
+++ result.js
@@ -0,0 +1,17 @@
+const find = document.getElementById("button-back");
+find.addEventListener("click", postBack);
+
+async function postBack() {
+    // const username = form.username.value;
+    // const fullname = form.fullname.value;
+    // const email = form.email.value;
+    // const password = form.password.value;
+    // const data = {
+    //     "username": username,
+    //     "fullname": fullname,
+    //     "email": email,
+    //     "password": password,
+    //     "role": ["ROLE_CLIENT"]
+    // };
+    window.location = 'index.html'
+}
