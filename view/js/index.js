fabc987b6bdeb61de9310b6e9f7192236fd6c1c9
adihendro <adi21hendro@gmail.com>
2021-11-28 19:48:53 +0700

    update priority selection

--- view/js/index.js
+++ view/js/index.js
@@ -1,67 +1,14 @@
 const find = document.getElementById("button-find");
-find.addEventListener("click", postFind);
 const form = document.querySelector("form");
+const prioritas1 = document.getElementById("prioritas1");
+const prioritas2 = document.getElementById("prioritas2");
+const prioritas3 = document.getElementById("prioritas3");
 
-const areas =
-    ["Setiabudi", "Dharmawangsa", "Thamrin", "Senopati", "SCBD", "Fatmawati", "Kemang", "Sudirman",
-        "Menteng", "Blok M", "Kuningan", "Melawai", "Bogor Utara", "Gandaria", "Cikini", "Senayan",
-        "Tebet", "Pluit", "Cilandak", "Kelapa Gading", "Bogor Timur", "Pantai Indah Kapuk", "Ancol",
-        "Karet", "Tanjung Duren", "Pondok Melati", "Cisarua", "Bendungan Hilir", "Tanah Sareal", "Jagakarsa",
-        "Serpong Utara", "Tanah Abang", "Pinang"
-    ]
-
-const types =
-    ["Casual Dining", "Fine Dining", "Café", "Lounge", "Quick Bites", "Bar", "Dessert Parlor", "Food Court", "Indonesian",
-        "Japanese", "Asian", "Desserts", "Steak", "Western", "Italian", "Peranakan", "Wine Bar", "Burger", "Cafe", "Coffee", "Korean",
-        "Grill", "Sushi", "Continental", "Jawa", "Spanish", "Fusion", "Betawi", "Seafood", "Ice Cream", "Pizza", "Bakery", "Arabian",
-        "Sunda", "BBQ", "European", "French", "Peruvian", "Ramen", "Finger Food", "German", "Indian", "American", "South American",
-        "Healthy Food", "Tea", "North Indian", "Australian", "Chinese", "Middle Eastern", "Dimsum", "Beverages", "Snacks", "Kebab",
-        "Moroccan", "Belanda", "Thai"
-    ]
-
-function populateLokasi(n) {
-    var ul = document.getElementById("lokasi");
-    populateOption(n, ul);
-}
-function populateJenis(n) {
-    var ul = document.getElementById("jenis");
-    populateOption(n, ul);
-}
-
-function populateOption(n, ul) {
-    var li = document.createElement("option");
-    li.appendChild(document.createTextNode(n));
-    li.setAttribute("value", n);
-    ul.appendChild(li);
-}
-
-// prioritas = ["Lokasi", "Jenis", "Rating"]
-
-// function populatePrioritas() {
-//     var ul = document.getElementsByClassName("prioritas");
-//     console.log(ul)
-//     var li = document.createElement("option");
-//     li.appendChild(document.createTextNode(prioritas[0]));
-//     li.appendChild(document.createTextNode(prioritas[1]));
-//     li.appendChild(document.createTextNode(prioritas[2]));
-//     li.setAttribute("value", prioritas[0]);
-//     li.setAttribute("value", prioritas[]);
-//     li.setAttribute("value", prioritas[2]);
-//     ul[0].appendChild(li);
-//     ul[1].appendChild(li);
-//     ul[2].appendChild(li);
-// }
-
-areas.forEach(populateLokasi)
-types.forEach(populateJenis)
-// populatePrioritas()
-
-
-
+find.addEventListener("click", postFind);
 async function postFind() {
     const lokasi = form.lokasi.value;
     const jenis = form.jenis.value;
-    const prioritas = [form.prioritas1.value, form.prioritas2.value, form.prioritas3.value];
+    const prioritas = [prioritas1.innerHTML, prioritas2.innerHTML, prioritas3.innerHTML];
     const data = {
         "lokasi": lokasi,
         "jenis": jenis,
@@ -69,7 +16,7 @@ async function postFind() {
         "prioritas2": prioritas[1],
         "prioritas3": prioritas[2],
     };
-    // console.log(data)
+    console.log(data)
 
-    window.location.href = '/result'
+    // window.location.href = '/result'
 }
